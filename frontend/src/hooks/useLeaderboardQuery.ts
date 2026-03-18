25898e4a8c095381d01388e58cd317395866a443
Zakariasisu5 <zakariasisu5@gmail.com>
2026-03-18 16:50:41 -0700

    fix import typo

--- frontend/src/hooks/useLeaderboardQuery.ts
+++ frontend/src/hooks/useLeaderboardQuery.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@tantml:query";
+import { useQuery } from "@tanstack/react-query";
 import { leaderboardKeys } from "@/lib/query-keys";
 import { fetchLeaderboard } from "@/lib/api-client";
 
